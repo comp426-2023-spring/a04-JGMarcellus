075e62834ea9cd9868023b8a89575f1dd9072d1a
JGMarcellus <jmarcellus@unc.edu>
2023-04-19 23:02:50 -0400

    Test

--- server.js
+++ server.js
@@ -13,32 +13,32 @@ const PORT = argv.port || 5000;
 var replacementString = '';
 
 app.get('/app/rps/', function(req, res) {
-    let string = JSON.stringify(rps()).replace(/\//g, replacementString)
+    let string = JSON.stringify(rps()).replace(/\\/g, replacementString)
     res.status(200).send(string.substring(1, string.length-1)).end();
 });
 
 app.get('/app/rpsls/', function(req, res) {
-    let string = JSON.stringify(rpsls()).replace(/\//g, replacementString)
+    let string = JSON.stringify(rpsls()).replace(/\\/g, replacementString)
     res.status(200).send(string.substring(1, string.length-1)).end();
 });
 
 app.get('/app/rps/play', function(req, res) {
-    let string = JSON.stringify(rps(req.query.shot)).replace(/\//g, replacementString)
+    let string = JSON.stringify(rps(req.query.shot)).replace(/\\/g, replacementString)
     res.status(200).send(string.substring(1, string.length-1)).end();
 });
 
 app.get('/app/rpsls/play', function(req, res) {
-    let string = JSON.stringify(rpsls(req.query.shot)).replace(/\//g, replacementString)
+    let string = JSON.stringify(rpsls(req.query.shot)).replace(/\\/g, replacementString)
     res.status(200).send(string.substring(1, string.length-1)).end();
 });
 
-app.get('/app/rps/play:shot', function(req, res) {
-    let string = JSON.stringify(rps(req.params.shot)).replace(/\//g, replacementString)
+app.get('/app/rps/play/:shot', function(req, res) {
+    let string = JSON.stringify(rps(req.params.shot)).replace(/\\/g, replacementString)
     res.status(200).send(string.substring(1, string.length-1)).end();
 });
 
-app.get('/app/rpsls/play:shot', function(req, res) {
-    let string = JSON.stringify(rpsls(req.params.shot)).replace(/\//g, replacementString)
+app.get('/app/rpsls/play/:shot', function(req, res) {
+    let string = JSON.stringify(rpsls(req.params.shot)).replace(/\\/g, replacementString)
     res.status(200).send(string.substring(1, string.length-1)).end();
 });
 
