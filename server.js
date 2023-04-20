694a3e45eb000b3c7439488133afed4696af0549
JGMarcellus <jmarcellus@unc.edu>
2023-04-19 22:47:57 -0400

    Test

---
+++ server.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+import { rpsls } from './lib/rpsls.js';
+import { rps } from './lib/rpsls.js';
+import minimist from 'minimist';
+import express from 'express'
+
+const app = express();
+
+var argv = minimist(process.argv.slice(2));
+const PORT = argv.port || 5000;
+
+app.get('/app/rps/', function(req, res) {
+    res.status(200).send(JSON.stringify(rps())).end();
+});
+
+app.get('/app/rpsls/', function(req, res) {
+    res.status(200).send(JSON.stringify(rpsls())).end();
+});
+
+app.get('/app/rps/play', function(req, res) {
+    res.status(200).send(JSON.stringify(rps(req.query.shot))).end();
+});
+
+app.get('/app/rpsls/play', function(req, res) {
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot))).end();
+});
+
+app.get('/app/rps/play:shot', function(req, res) {
+    res.status(200).send(JSON.stringify(rps(req.params.shot))).end();
+});
+
+app.get('/app/rpsls/play:shot', function(req, res) {
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot))).end();
+});
+
+app.get("/app", function(req, res) {
+    res.status(200).send("200 OK").end();
+});
+
+
+app.all('*', (req, res) => {
+    res.status(404).send('404 NOT FOUND').end();
+});
+
+app.listen(PORT, function(err){
+    if (err) console.log(err);
+ });
