edce3155cfb2dc804ebab127e3fb2bcfdaf60dee
JGMarcellus <jmarcellus@unc.edu>
2023-04-19 23:00:02 -0400

    Test

--- server.js
+++ server.js
@@ -10,30 +10,36 @@ const app = express();
 var argv = minimist(process.argv.slice(2));
 const PORT = argv.port || 5000;
 
-replacementString = '';
+var replacementString = '';
 
 app.get('/app/rps/', function(req, res) {
-    res.status(200).send(JSON.stringify(rps()).replace(/\//g, replacementString)).end();
+    let string = JSON.stringify(rps()).replace(/\//g, replacementString)
+    res.status(200).send(string.substring(1, string.length-1)).end();
 });
 
 app.get('/app/rpsls/', function(req, res) {
-    res.status(200).send(JSON.stringify(rpsls()).replace(/\//g, replacementString)).end();
+    let string = JSON.stringify(rpsls()).replace(/\//g, replacementString)
+    res.status(200).send(string.substring(1, string.length-1)).end();
 });
 
 app.get('/app/rps/play', function(req, res) {
-    res.status(200).send(JSON.stringify(rps(req.query.shot)).replace(/\//g, replacementString)).end();
+    let string = JSON.stringify(rps(req.query.shot)).replace(/\//g, replacementString)
+    res.status(200).send(string.substring(1, string.length-1)).end();
 });
 
 app.get('/app/rpsls/play', function(req, res) {
-    res.status(200).send(JSON.stringify(rpsls(req.query.shot)).replace(/\//g, replacementString)).end();
+    let string = JSON.stringify(rpsls(req.query.shot)).replace(/\//g, replacementString)
+    res.status(200).send(string.substring(1, string.length-1)).end();
 });
 
 app.get('/app/rps/play:shot', function(req, res) {
-    res.status(200).send(JSON.stringify(rps(req.params.shot)).replace(/\//g, replacementString)).end();
+    let string = JSON.stringify(rps(req.params.shot)).replace(/\//g, replacementString)
+    res.status(200).send(string.substring(1, string.length-1)).end();
 });
 
 app.get('/app/rpsls/play:shot', function(req, res) {
-    res.status(200).send(JSON.stringify(rpsls(req.params.shot)).replace(/\//g, replacementString)).end();
+    let string = JSON.stringify(rpsls(req.params.shot)).replace(/\//g, replacementString)
+    res.status(200).send(string.substring(1, string.length-1)).end();
 });
 
 app.get("/app", function(req, res) {
