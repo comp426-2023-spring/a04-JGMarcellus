e8d9d9eecdb99a900757c760e2032ad19a00e1cd
JGMarcellus <jmarcellus@unc.edu>
2023-04-19 23:41:48 -0400

    Final

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import minimist from 'minimist';
 import express from 'express'
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 var argv = minimist(process.argv.slice(2));
 const PORT = argv.port || 5000;
@@ -22,12 +24,12 @@ app.get('/app/rpsls/', function(req, res) {
     res.status(200).send(string.substring(1, string.length-1)).end();
 });
 
-app.get('/app/rps/play/', function(req, res) {
+app.get('/app/rps/play', function(req, res) {
     let string = JSON.stringify(rps(req.query.shot)).replace(/\\/g, replacementString)
     res.status(200).send(string.substring(1, string.length-1)).end();
 });
 
-app.get('/app/rpsls/play/', function(req, res) {
+app.get('/app/rpsls/play', function(req, res) {
     let string = JSON.stringify(rpsls(req.query.shot)).replace(/\\/g, replacementString)
     res.status(200).send(string.substring(1, string.length-1)).end();
 });
@@ -49,8 +51,6 @@ app.get("/app/", function(req, res) {
 app.get("*", function(req, res) {
     res.status(404).send("404 NOT FOUND").end();
 });
-
-
 
 app.listen(PORT, function(err){
     if (err) console.log(err);
